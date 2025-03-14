2b39b6d20514f9b71b40c56369777b92eb0d7854
drist37 <vasilieva.e@live.unic.ac.cy>
2025-03-14 09:55:03 +0200

    most of draw shapes is done

--- lab0.cpp
+++ lab0.cpp
@@ -6,6 +6,8 @@
 //
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 //global variables
@@ -18,14 +20,19 @@ void drawSquare(int length, char ch = symbol);
 void drawSquareFilled(int length, char ch = symbol);
 void drawRectangle(int length, int height, char ch = symbol);
 void drawRectangleFilled(int length, int height, char ch = symbol);
+void drawShapes(int numShapes);
 
 
 
 int main() {
 
+    const int MAX_SHAPES = 10;
+    
     int choice;
     int length = 0;
     int height = 0;
+    
+    srand(static_cast<unsigned int>(time(NULL)));
     
     do {
         cout << "\n1) Draw a horizontal line";
@@ -34,7 +41,8 @@ int main() {
         cout << "\n4) Draw a square filled";
         cout << "\n5) Draw a rectangle";
         cout << "\n6) Draw a rectangle filled";
-        cout << "\n7) Quit";
+        cout << "\n7) Draw a random number of shapes";
+        cout << "\n8) Quit";
         cout << "\n\nEnter your choice: ";
 
         cin >> choice;
@@ -53,6 +61,8 @@ int main() {
         }else if (choice == 2){
             cout << "\nEnter the height of the line: ";
             cin >> height;
+            cout << "\nEnter symbol to use: ";
+            cin >> symbol;
             
             if (height <= 0){
                 cout << "Height cannot be 0 or less\n";
@@ -63,6 +73,9 @@ int main() {
             cout << "\nEnter the dimension of the square: ";
             cin >> length;
             
+            cout << "\nEnter symbol to use: ";
+            cin >> symbol;
+            
             if (length <= 0){
                 cout << "Dimensions cannot be 0 or less\n";
             }else{
@@ -71,29 +84,43 @@ int main() {
         }else if (choice == 4){
             cout << "\nEnter the dimension of the square: ";
             cin >> length;
+            
+            cout << "\nEnter symbol to use: ";
+            cin >> symbol;
+            
             drawSquareFilled(length);
         }else if (choice == 5){
             cout << "\nEnter the length of the rectangle: ";
             cin >> length;
             cout << "\nEnter the height of the rectangle: ";
             cin >> height;
+            
+            cout << "\nEnter symbol to use: ";
+            cin >> symbol;
+            
             drawRectangle(length, height);
         }else if (choice == 6){
             cout << "\nEnter the length of the rectangle: ";
             cin >> length;
             cout << "\nEnter the height of the rectangle: ";
             cin >> height;
+            
+            cout << "\nEnter symbol to use: ";
+            cin >> symbol;
+            
             drawRectangleFilled(length, height);
         }else if (choice == 7){
+            drawShapes(MAX_SHAPES);
+        }else if (choice == 8){
             return 0;
         }else {
-            cout << "\nInvalid choice\n";
+            cerr << "\nInvalid choice\n";
         }
         
-    } while (choice != 7);
+    } while (choice != 8);
     
 
-    cout << "Have a nice day!";
+    cout << "\n7Have a nice day!\n";
 
 
     return 0;
@@ -180,4 +207,39 @@ void drawRectangleFilled(int length, int height, char ch){
     }
 }
 
+//draws a random number of shapes, i.e. automated testing function
+void drawShapes(int numShapes) {
+    int shapeType;
+    int shapeLength;
+    int shapeChar;
+    
+    cout << "\nDraws " << numShapes << " random shapes";
+    
+    shapeType = 1 + rand() % 6;
+    shapeLength = 5 + rand() % 16; //@todo remove 5, 16, 33 and 94
+    shapeChar = 33 + rand() % 127 - 33; //printable ascii characters
+    
+    for (int i = 0; i <= numShapes; i++){
+    switch (shapeType) {
+    case 1:
+            drawHorizontalLine(shapeLength, shapeChar);
+            break;
+    case 2:
+            drawVerticalLine(shapeLength, shapeChar);
+            break;
+    case 3:
+            break;
+    case 4:
+            break;
+    case 5:
+            drawRectangle(shapeLength + 3, shapeLength, shapeChar);
+            break;
+    case 6:
+            break;
+    default:
+            cerr << "Should not happen";
+            break;
+    }
+    }
+}
 
