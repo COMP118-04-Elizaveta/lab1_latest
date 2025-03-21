5cd4c5942d0fe97dbd8c774249f07a1cd96ebf43
drist37 <vasilieva.e@live.unic.ac.cy>
2025-03-21 10:20:06 +0200

    Recompiled the Doxygen documentation with the finished function comments

--- doc/html/lab0_8cpp.js
+++ doc/html/lab0_8cpp.js
@@ -1,7 +1,12 @@
 var lab0_8cpp =
 [
+    [ "drawHorizontalLine", "lab0_8cpp.html#a6d0b0646b1685e178af5bc872e662944", null ],
     [ "drawRectangle", "lab0_8cpp.html#ad8519dd4b292b22c2b539b7c5548d322", null ],
     [ "drawRectangleFilled", "lab0_8cpp.html#a7c7c3909392190b277a66285503ec0dd", null ],
+    [ "drawShapes", "lab0_8cpp.html#a90c5b61205a5ed02e9901c05bb9f7689", null ],
+    [ "drawSquare", "lab0_8cpp.html#a1109298b032654ff9dc032f62b2354d8", null ],
+    [ "drawSquareFilled", "lab0_8cpp.html#aa23d950afafafc1b4bd6acf76a13d01e", null ],
+    [ "drawVerticalLine", "lab0_8cpp.html#a0a21a08bb0790036f800f59580d5d515", null ],
     [ "main", "lab0_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4", null ],
     [ "symbol", "lab0_8cpp.html#af7565614b2ea8e3f98b39303ae09ebca", null ]
 ];
--- doc/html/search/functions_0.js
+++ doc/html/search/functions_0.js
@@ -1,5 +1,10 @@
 var searchData=
 [
-  ['drawrectangle_0',['drawRectangle',['../lab0_8cpp.html#ad8519dd4b292b22c2b539b7c5548d322',1,'lab0.cpp']]],
-  ['drawrectanglefilled_1',['drawRectangleFilled',['../lab0_8cpp.html#a7c7c3909392190b277a66285503ec0dd',1,'lab0.cpp']]]
+  ['drawhorizontalline_0',['drawHorizontalLine',['../lab0_8cpp.html#a6d0b0646b1685e178af5bc872e662944',1,'lab0.cpp']]],
+  ['drawrectangle_1',['drawRectangle',['../lab0_8cpp.html#ad8519dd4b292b22c2b539b7c5548d322',1,'lab0.cpp']]],
+  ['drawrectanglefilled_2',['drawRectangleFilled',['../lab0_8cpp.html#a7c7c3909392190b277a66285503ec0dd',1,'lab0.cpp']]],
+  ['drawshapes_3',['drawShapes',['../lab0_8cpp.html#a90c5b61205a5ed02e9901c05bb9f7689',1,'lab0.cpp']]],
+  ['drawsquare_4',['drawSquare',['../lab0_8cpp.html#a1109298b032654ff9dc032f62b2354d8',1,'lab0.cpp']]],
+  ['drawsquarefilled_5',['drawSquareFilled',['../lab0_8cpp.html#aa23d950afafafc1b4bd6acf76a13d01e',1,'lab0.cpp']]],
+  ['drawverticalline_6',['drawVerticalLine',['../lab0_8cpp.html#a0a21a08bb0790036f800f59580d5d515',1,'lab0.cpp']]]
 ];
